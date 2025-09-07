15882a909083e8bebe19e41053baffa399942bd0
zmykedev <zmaikol399@gmail.com>
2025-09-07 02:11:32 -0300

    delete terser options

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,6 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'esbuild',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks: {
